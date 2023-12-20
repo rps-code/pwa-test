0a9d3bf4125861d78c71d8b4127be67959f2ff10
Karl Chelton <reps.bh@protonmail.com>
2023-12-20 16:33:42 +0000

    fix

--- api/index.js
+++ api/index.js
@@ -6,12 +6,12 @@ const app = express();
 app.use(cors());
 
 app.get('/api/exampleMessage', (req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+    // res.setHeader('Content-Type', 'text/html');
+    // res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
 
     const randomNumber = Math.floor(Math.random() * 100);
 
-    res.json({ message: `I am exampleMessage woooo ${randomNumber}` });
+    res.json({ message: `I am exampleMessage woohoo ${randomNumber}` });
 });
 
 app.listen(3000, () => {
