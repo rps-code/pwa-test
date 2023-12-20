683788d8f2703e56fc7c394c59d5cd8335df9045
Karl Chelton <reps.bh@protonmail.com>
2023-12-20 15:13:09 +0000

    fix

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,17 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
+const app = createApp(App);
+
 if ('serviceWorker' in navigator) {
 	window.addEventListener('load', () => {
-	navigator.serviceWorker.register('/src/serviceWorker.ts')
-		.then(registration => {
-			console.log('SW registered:', registration);
+		navigator.serviceWorker.register('/serviceWorker.js').then(registration => {
+			console.log('SW registered: ', registration);
 		}).catch(registrationError => {
-			console.log('SW registration failed:', registrationError);
+			console.error('SW registration failed: ', registrationError);
 		});
 	});
 }
-
-const app = createApp(App);
 
 app.use(router);
 
